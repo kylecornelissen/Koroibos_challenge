a77e86e698b2a7312bd46d2b4586fd8ae191ec4b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-09-17 22:02:29 -0600

    Add age_sorter method to olympian model

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,3 +1,8 @@
 class Olympian < ApplicationRecord
-  
+  def self.age_sorter(youngest_or_oldest, order = "DESC")
+    order = "ASC" if youngest_or_oldest == "youngest"
+    select(:name, :team, :age, :sport)
+    .order("age #{order}")
+    .first
+  end
 end
--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Olympian, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+
+  end
+
+  describe 'class methods' do
+    before :each do
+      @o1 = create(:olympian, name: 'Freshman', age: '24')
+      @o1 = create(:olympian, name: 'Junior', age: '33')
+      @o1 = create(:olympian, name: 'Senior', age: '45')
+    end
+
+    it '.age_sorter' do
+      youngest_olympian = Olympian.age_sorter("youngest")
+      oldest_olympian = Olympian.age_sorter("oldest")
+
+      expect(youngest_olympian[:name]).to eq('Freshman')
+      expect(oldest_olympian[:name]).to eq('Senior')
+    end
+  end
 end
