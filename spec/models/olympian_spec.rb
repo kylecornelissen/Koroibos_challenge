836c1a69d87973a481baea99b9848ba71726aa7a
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-09-18 00:37:04 -0600

    Add relationships to olympian model

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Olympian, type: :model do
   describe 'relationships' do
-
+    it { should have_many :olympian_events }
+    it { should have_many(:events).through(:olympian_events) }
+    it { should have_many(:medals).through(:olympian_events) }
   end
 
   describe 'instance methods' do
