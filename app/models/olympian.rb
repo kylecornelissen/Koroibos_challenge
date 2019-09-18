2dba2ac80a33a5762724d43c2ffc02367fbb6e92
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-09-17 23:23:18 -0600

    Add olympian stats instance method and average weight class method

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,8 +1,24 @@
 class Olympian < ApplicationRecord
+
+  def olympian_stats
+    { "total_competiting_olympians" => Olympian.count,
+      "average_weight" => {
+        "unit" => "kg",
+        "male_olympians" => Olympian.average_weight("M").round(2).to_f,
+        "female_olympians" => Olympian.average_weight("F").round(2).to_f
+      },
+      "average_age" => Olympian.average(:age).round(2).to_f
+    }
+  end
+
   def self.age_sorter(youngest_or_oldest, order = "DESC")
     order = "ASC" if youngest_or_oldest == "youngest"
     select(:name, :team, :age, :sport)
     .order("age #{order}")
     .first
+  end
+
+  def self.average_weight(sex)
+    select(:weight, :sex).where(sex: sex).average(:weight)
   end
 end
